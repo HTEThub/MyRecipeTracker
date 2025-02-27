8b4c0fe6742a6b50be774aa98e949b6c8a815315
HTET <htetgamer2020@gmail.com>
2025-02-27 15:02:09 +1000

    Initial commit

---
+++ app/src/main/java/com/htet08/htetrecipetracker/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.htet08.htetrecipetracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.htet08.htetrecipetracker.ui.theme.HtetRecipeTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HtetRecipeTrackerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainLayout(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainLayout(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainLayoutPreview() {
+    HtetRecipeTrackerTheme {
+        MainLayout("Android")
+    }
+}
